5f78c767217d5d39de04a1ce398c532995fb6b24
Michael Eluzai Situmorang <michaeleluzai@student.telkomuniversity.ac.id>
2026-09-21 13:07:09 +0700

    1.1

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nusantara Cerdas',
       debugShowCheckedModeBanner: false,
+
+      // 1. Konfigurasi Tema Terang Material 3
       theme: ThemeData(
-        colorSchemeSeed: Colors.teal, // Tema warna kota
+        colorSchemeSeed: Colors.teal,
         useMaterial3: true,
+        brightness: Brightness.light,
       ),
+
+      // 2. Konfigurasi Tema Gelap Material 3
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.teal,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+
+      // 3. Otomatis mengikuti pengaturan sistem perangkat (Terang/Gelap)
+      themeMode: ThemeMode.system,
+
       home: const HalamanUtama(),
     );
   }
